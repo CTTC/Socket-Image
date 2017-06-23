6b9919f01a1c52b417fc4784610385e225187e47
CTTC <chentao904@163.com>
2017-06-23 14:46:35 +0800

    Add example of connecting to remote machine by ip address

--- client.cpp
+++ client.cpp
@@ -10,7 +10,7 @@
 #include <opencv2/opencv.hpp>
 #include <chrono>
 #include <ctime>
-
+#include <arpa/inet.h> 
 #define LOCALHOST "localhost"
 #define PORT 7200
 
@@ -28,7 +28,7 @@ bool recv_all(int socket, void *buffer, int length)
   {
     puts("rece");
     bytes = recv(socket, ptr, length, 0);
-    std::cout << "bytes " << bytes << "  length: " << length<< std::endl;
+    std::cout << "bytes " << bytes << "  length: " << length << std::endl;
     if (bytes < 1) return false;
     ptr += bytes;
     length -= bytes;
@@ -37,19 +37,19 @@ bool recv_all(int socket, void *buffer, int length)
 }
 bool send_all(int socket, void *buffer, int length)
 {
-    uchar *ptr = (uchar*) buffer;
-    int bytes = 0;
-    while (length > 0)
-    {
-        bytes = send(socket, ptr, length, 0);
-        if (bytes < 1) return false;
-        ptr += bytes;
-        length -= bytes;
-    }
-    return true;
+  uchar *ptr = (uchar*) buffer;
+  int bytes = 0;
+  while (length > 0)
+  {
+    bytes = send(socket, ptr, length, 0);
+    if (bytes < 1) return false;
+    ptr += bytes;
+    length -= bytes;
+  }
+  return true;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
   int sockfd, portno, n, imgSize, IM_HEIGHT, IM_WIDTH;
   struct sockaddr_in serv_addr;
@@ -60,21 +60,36 @@ int main()
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   if (sockfd < 0) error("ERROR opening socket");
+  // // If you want to connect to the localhost
+  // server = gethostbyname(LOCALHOST);
 
-  server = gethostbyname(LOCALHOST);
+  // if (server == NULL) {
+  //   fprintf(stderr, "ERROR, no such host\n");
+  //   exit(0);
+  // }
 
-  if (server == NULL) {
-    fprintf(stderr, "ERROR, no such host\n");
-    exit(0);
+  // bzero((char *) &serv_addr, sizeof(serv_addr));
+  // serv_addr.sin_family = AF_INET;
+  // bcopy((char *)server->h_addr,
+  //       (char *)&serv_addr.sin_addr.s_addr,
+  //       server->h_length);
+  // serv_addr.sin_port = htons(portno);
+
+  // If you want to connect to a machine by its ip address
+  if (argc != 2)
+  {
+    printf("\n Usage: %s <ip of server> \n", argv[0]);
+    return 1;
   }
-
   bzero((char *) &serv_addr, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
-  bcopy((char *)server->h_addr,
-        (char *)&serv_addr.sin_addr.s_addr,
-        server->h_length);
   serv_addr.sin_port = htons(portno);
 
+  if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
+  {
+    printf("\n inet_pton error occured\n");
+    return 1;
+  }
   if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
     error("ERROR connecting");
 
@@ -120,9 +135,9 @@ int main()
     if (!rev_ok) error("ERROR receiving feedback");
     // int bytes = recv(sockfd, received_str, sizeof(received_str), 0);
     // if (bytes == -1) error("recv failed");
-    std::cout << " Testing Feedback Received: " << received_str[0] << "  " << 
-    received_str[1] << "  " << received_str[2]<< std::endl;
-    
+    std::cout << " Testing Feedback Received: " << received_str[0] << "  " <<
+              received_str[1] << "  " << received_str[2] << std::endl;
+
     puts("  Feedback Received");
   }
   end = std::chrono::system_clock::now();
